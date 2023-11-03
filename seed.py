11777a5bd18ad5f0ef31e0af72246e26e71db5df
calvincarter <calvincarter.160@gmail.com>
2023-11-03 07:30:33 -0500

    fix seed

--- seed.py
+++ seed.py
@@ -1,8 +1,7 @@
 """Seed database with sample data from CSV Files."""
 
 from csv import DictReader
-from app import db
-from models import User, Message, Follows
+from models import User, Message, Follows, db
 
 
 db.drop_all()
